29daf6a3352f2849b2bd76523eba6b698d28ceb1
Sameer Desai <sameerdesai1008@gmail.com>
2020-06-06 16:44:23 +0530

    button options

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
-    <img src="./assets/logo.png" alt="Chat Icon">
+    <router-view />
+    <img src="./assets/logo.png" alt="Chat Icon" />
     <VueBotUI
-    :options="botOptions"
-    :messages="messageData"
-    :bot-typing="botTyping"
-    :input-disable="inputDisable"
-    :is-open="false"
-    @init="botStart"
-    @msg-send="msgSend"
+      :options="botOptions"
+      :messages="messageData"
+      :bot-typing="botTyping"
+      :input-disable="inputDisable"
+      :is-open="false"
+      @init="botStart"
+      @msg-send="msgSend"
+      @destroy="deleteSession"
     />
   </div>
 </template>
@@ -26,10 +27,15 @@ export default {
   data () {
     return {
       messageData: [],
+      sendMsg: '',
       messageService: null,
       botTyping: false,
+      lexUserId: '',
+      sessionAttributes: {},
       inputDisable: false,
       botOptions: {
+        botTitle: 'Order a Pizza',
+        colorScheme: '#FFA500',
         botAvatarImg: BotIcon,
         boardContentBg: '#f4f4f4',
         msgBubbleBgBot: '#fff',
@@ -56,11 +62,35 @@ export default {
         text: 'Hello'
       })
     },
+    deleteSession () {
+      if (this.messageData.length === 1) {
+        return
+      }
+      var params = {
+        botAlias: 'PizzaLexBot' /* required */,
+        botName: 'pizzabot' /* required */,
+        userId: this.lexUserId /* required */
+      }
+      this.messageService.deleteSession(params, (err, data) => {
+        if (err) console.log(err, err.stack)
+        // an error occurred\
+        else console.log('Deleted Session : ', data) // successful response
+      })
+      this.messageData = []
+      this.lexUserId = ''
+    },
     configService () {
       var AWS = require('aws-sdk')
-      console.log(AWS)
-      console.log(process.env.VUE_APP_SOMEKEY)
-      this.messageService = {} // chatbot service API
+      AWS.config.region = process.env.VUE_APP_REGION // Region
+      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        // Provide your Pool Id here
+        IdentityPoolId: process.env.VUE_APP_IdentityPoolId
+      })
+
+      var lexruntime = new AWS.LexRuntime()
+      this.lexUserId = 'chatbot-demo' + Date.now()
+      this.sessionAttributes = {}
+      this.messageService = lexruntime // chatbot service API
     },
 
     msgSend (value) {
@@ -70,8 +100,17 @@ export default {
         type: 'text',
         text: value.text
       })
-
+      this.sendMsg = value.text
       this.getResponse()
+      this.sendMsg = ''
+    },
+    getDisplayOptions (options) {
+      const opt = []
+      options.forEach(element => {
+        element.action = 'postback'
+        opt.push(element)
+      })
+      return opt
     },
 
     // Submit the message from user to bot API, then get the response from Bot
@@ -79,25 +118,57 @@ export default {
       // Loading
       this.botTyping = true
 
+      var replyMessage = {}
+      var params = {
+        botAlias: 'PizzaLexBot', // '$LATEST',
+        botName: 'pizzabot',
+        inputText: this.sendMsg,
+        userId: this.lexUserId,
+        sessionAttributes: this.sessionAttributes
+      }
+
       // Post the message from user here
-      // Then get the response as below
-
-      // Create new message from fake data
-      this.messageService.createMessage().then(response => {
-        const replyMessage = {
-          agent: 'bot',
-          ...response
+      this.messageService.postText(params, (err, data) => {
+        if (err) {
+          console.log(err, err.stack)
+          console.log('Error:  ' + err.message + ' (see console for details)')
         }
-
-        this.inputDisable = response.disableInput
-        this.messageData.push(replyMessage)
-
-        // finish
-        this.botTyping = false
+        if (data) {
+          // capture the sessionAttributes for the next cycle
+          this.sessionAttributes = data.sessionAttributes
+          // show response and/or error/dialog status
+          console.log('Response : ', data)
+          // Then get the response as below
+          if (typeof data.responseCard === 'undefined') {
+            replyMessage = {
+              agent: 'bot',
+              type: 'text',
+              disableInput: false,
+              text: data.message
+            }
+          } else {
+            console.log('Response Card :', data.responseCard.genericAttachments[0])
+            replyMessage = {
+              agent: 'bot',
+              type: 'button',
+              text: data.message,
+              disableInput: true,
+              options: this.getDisplayOptions(data.responseCard.genericAttachments[0].buttons)
+            }
+          }
+          console.log('Got Reply Now')
+          console.log('Pushing Message Now')
+          this.messageData.push(replyMessage)
+          this.inputDisable = replyMessage.disableInput
+          // finish
+          this.botTyping = false
+        }
       })
+      // this.inputDisable = response.disableInput
     }
   }
 }
+
 </script>
 <style>
 #app {
